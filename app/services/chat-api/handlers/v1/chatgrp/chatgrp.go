7e223b03e6cd88709b9a3e3d0f361a710e823788
Anton Nikolaev <regwebghost@yandex.ru>
2023-05-30 20:27:32 +0300

    Refactoring

--- app/services/chat-api/handlers/v1/chatgrp/chatgrp.go
+++ app/services/chat-api/handlers/v1/chatgrp/chatgrp.go
@@ -15,6 +15,10 @@ import (
 )
 
 const (
+	searchPairTimeoutError = "pair search time out reached: pair can not be found"
+)
+
+const (
 	// bufferMaxMessages defines max size of the message channel for a subscriber
 	bufferMaxMessages = 10
 
@@ -31,17 +35,17 @@ const (
 	checkSubAvailabilityMaxTimeSeconds = 5
 )
 
-// subscriber represents a subscriber.
-// Messages are sent on the messages channel and can be represented as any object.
-type subscriber struct {
-	id       string
-	pairId   string
-	messages chan any
-}
-
 // message represents a message.
 type message struct {
 	Content string `json:"content"`
+}
+
+// subscriber represents a subscriber.
+// Messages are sent on the messages channel and can be represented as message type.
+type subscriber struct {
+	id       string
+	pairId   string
+	messages chan *message
 }
 
 // Handlers manages the set of chat endpoints.
@@ -69,8 +73,8 @@ func New(log *zap.SugaredLogger) *Handlers {
 	}
 }
 
-// publishMessage publishes a message to the message channel of a paired subscriber.
-func (h *Handlers) publishMessage(msg any, s *subscriber) error {
+// publishMessage publishes a message from a subscriber to the corresponding subscriber pair.
+func (h *Handlers) publishMessage(msg *message, s *subscriber) error {
 	h.subscribersMu.Lock()
 	defer h.subscribersMu.Unlock()
 
@@ -78,36 +82,41 @@ func (h *Handlers) publishMessage(msg any, s *subscriber) error {
 		return fmt.Errorf("message %s can not be published, limiter error: %w", msg, err)
 	}
 
-	// Message must be published to the paired subscriber only
-	sAnother, ok := h.subscribers[s.pairId]
+	// Message can be published to the paired subscriber only and only if pair exists
+	sPair, ok := h.subscribers[s.pairId]
 	if !ok {
-		return fmt.Errorf("message %s can not be published, no available pair to send message", msg)
+		return fmt.Errorf("message %s can not be published, no available pair to send message to", msg)
 	}
 
-	sAnother.messages <- msg
-	h.log.Debugw("message sent successfully", "subscriberId", s.id, "anotherId", sAnother.id)
+	sPair.messages <- msg
+	h.log.Debugw("message sent successfully", "fromId", s.id, "toId", sPair.id)
 
 	return nil
 }
 
-// linkSubscribersById links two available subscribers with each other as a pair.
-func (h *Handlers) linkSubscribersById(sId, anotherId string) {
+// notifySubscriber notifies subscriber directly about service state changes.
+func (h *Handlers) notifySubscriber(ctx context.Context, conn *websocket.Conn, sId string, content string) {
+	msg := &message{Content: content}
+	if err := wsjson.Write(ctx, conn, msg); err != nil {
+		h.log.Warnw("subscriber can not be notified due to error", "subscriberId", sId, "error", err)
+	}
+}
+
+// linkSubscribers links two available subscribers with each other as a pair.
+func (h *Handlers) linkSubscribers(sId, sPairId string) {
 	h.subscribersMu.Lock()
 	defer h.subscribersMu.Unlock()
 
-	h.subscribers[anotherId].pairId = sId
-	h.subscribers[sId].pairId = anotherId
+	h.subscribers[sPairId].pairId = sId
+	h.subscribers[sId].pairId = sPairId
 
-	h.log.Debugw("successfully linkeds two subscribers in a pair",
-		"subscriberId", h.subscribers[sId].id,
-		"subscriberPairId", h.subscribers[sId].pairId,
-		"anotherId", h.subscribers[anotherId].id,
-		"anotherPairId", h.subscribers[anotherId].pairId,
-		"status", "success")
+	h.log.Debugw("successfully linked two subscribers",
+		"firstId", h.subscribers[sId].id,
+		"secondId", h.subscribers[sId].pairId)
 }
 
-// assignAvailableSubscriber assigns an available (if any) subscriber for a new subscriber.
-func (h *Handlers) assignAvailableSubscriber(ctx context.Context, s *subscriber) error {
+// assignSubscriber assigns an available subscriber to a newcomer subscriber.
+func (h *Handlers) assignSubscriber(ctx context.Context, s *subscriber) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -118,27 +127,26 @@ func (h *Handlers) assignAvailableSubscriber(ctx context.Context, s *subscriber)
 
 	go func() {
 		for range ticker.C {
-			// If the current subscriber already has a pair - success, pair exists
+			// Stop search If the current subscriber already has an assigned pair, mark as found
 			if s.pairId != "" {
-				h.log.Debugw("pair for a subscriber exists", "subscriberId", s.id, "pairId", s.pairId, "status", "success")
+				h.log.Debugw("subscriber has a pair", "subscriberId", s.id, "pairId", s.pairId)
 				isFound <- true
 				return
 			}
-
 			select {
 			case <-ctx.Done():
 				h.log.Debugw("pair search is finished (found or timed out)", "subscriberId", s.id, "reason", "foundOrTimeout")
 				return
 			default:
-				h.log.Debugw("searching for an available pair to assign for a subscriber", "subscriberId", s.id, "status", "inProgress")
+				h.log.Debugw("search for a pair to assign to a subscriber", "subscriberId", s.id)
 				for idAnother, sAnother := range h.subscribers {
-					// If another subscriber already has a pair (not available) or it is the current subscriber - skip
+					// Skip another subscriber if he has a pair or if it is a current subscriber
 					if sAnother.pairId != "" || idAnother == s.id {
 						continue
 					}
 
-					// Otherwise, the current subscriber and another one are both available to be linked
-					h.linkSubscribersById(s.id, sAnother.id)
+					// The current subscriber and another one are both able to be linked with each other, mark as found
+					h.linkSubscribers(s.id, sAnother.id)
 					isFound <- true
 				}
 			}
@@ -147,15 +155,15 @@ func (h *Handlers) assignAvailableSubscriber(ctx context.Context, s *subscriber)
 
 	select {
 	case <-isFound:
-		h.log.Debugw("pair was found", "subscriberId", s.id, "status", "success")
+		h.log.Debugw("pair found", "subscriberId", s.id, "pairId", s.pairId)
 		return nil
 	case <-time.After(checkSubAvailabilityMaxTimeSeconds * time.Second):
-		h.log.Debugw("pair search time out reached", "subscriberId", s.id, "status", "failure")
-		return errors.New("pair search time out reached")
+		h.log.Debugw(searchPairTimeoutError, "subscriberId", s.id)
+		return errors.New(searchPairTimeoutError)
 	}
 }
 
-// addSubscriber registers a subscriber.
+// addSubscriber adds a subscriber.
 func (h *Handlers) addSubscriber(s *subscriber) {
 	h.subscribersMu.Lock()
 	h.subscribers[s.id] = s
@@ -165,20 +173,11 @@ func (h *Handlers) addSubscriber(s *subscriber) {
 // deleteSubscriber deletes the given subscriber.
 func (h *Handlers) deleteSubscriber(s *subscriber) {
 	h.subscribersMu.Lock()
-	defer h.subscribersMu.Unlock()
-
-	// If the current subscriber has a pair, make the pair available for a new subscriber (since the current is not
-	// active anymore). Otherwise, subscriber was not able to find a pair, so nothing to delete here
-	sAnother, ok := h.subscribers[s.pairId]
-	if ok {
-		sAnother.pairId = ""
-		h.log.Debugw("pair subscriber is free now", "subscriberId", s.id, "subscriberPairId", s.pairId)
-	}
-
 	delete(h.subscribers, s.id)
+	defer h.subscribersMu.Unlock()
 }
 
-// readMessages reads messages from a client and publish them to the message channel when available.
+// readMessages reads messages from a subscriber connection and publishes them to the message channel.
 func (h *Handlers) readMessages(ctx context.Context, conn *websocket.Conn, s *subscriber) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 
@@ -189,24 +188,25 @@ func (h *Handlers) readMessages(ctx context.Context, conn *websocket.Conn, s *su
 				h.log.Debugw("read context is done", "subscriberId", s.id)
 				return
 			default:
-				var msg interface{}
+				msg := &message{}
 				if err := wsjson.Read(ctx, conn, &msg); err != nil {
+					// Stop the read and write context if subscriber is closed the connection
 					if errors.As(err, &websocket.CloseError{}) {
-						h.log.Warnw("message can not be read, client closed the connection", "subscriberId", s.id)
+						h.log.Warnw("message can not be read, subscriber closed the connection", "subscriberId", s.id)
 						cancel()
-						continue
+						return
 					}
 
+					// Skip the message if it can not be read or processed; take the next one
 					h.log.Warnw("message can not be read due to error", "subscriberId", s.id, "error", err)
-					// Skip this message, try to take the next one
 					continue
 				}
 
-				h.log.Debugw("read message from a subscriber", "subscriberId", s.id, "message", msg)
+				h.log.Debugw("successfully read the message from a subscriber", "subscriberId", s.id, "message", msg)
 
 				if err := h.publishMessage(msg, s); err != nil {
+					// Skip the message if it can not be published or processed; take the next one
 					h.log.Warnw("message can not be published due to error", "subscriberId", s.id, "error", err)
-					// Skip this message, try to take the next one
 					continue
 				}
 			}
@@ -216,18 +216,18 @@ func (h *Handlers) readMessages(ctx context.Context, conn *websocket.Conn, s *su
 	return ctx
 }
 
-// writeMessages writes messages from the message channel to the client
+// writeMessages writes messages from the message channel to the subscriber connection.
 func (h *Handlers) writeMessages(ctx context.Context, conn *websocket.Conn, s *subscriber) {
 	go func() {
 		for {
 			select {
 			case msg := <-s.messages:
-				h.log.Debugw("write message to a subscriber", "subscriberId", s.id, "message", msg)
 				if err := wsjson.Write(ctx, conn, msg); err != nil {
+					// Skip the message if it can not be written or processed; take the next one
 					h.log.Warnw("message can not be written due to error", "subscriberId", s.id, "error", err)
-					// Skip this message, try to take the next one
 					continue
 				}
+				h.log.Debugw("successfully wrote message to a subscriber", "subscriberId", s.id, "message", msg)
 			case <-ctx.Done():
 				h.log.Debugw("write context is done", "subscriberId", s.id)
 				return
@@ -236,16 +236,8 @@ func (h *Handlers) writeMessages(ctx context.Context, conn *websocket.Conn, s *s
 	}()
 }
 
-// notifySubscriber notifies subscriber about any service-related actions
-func (h *Handlers) notifySubscriber(ctx context.Context, conn *websocket.Conn, sId string, msg string) {
-	if err := wsjson.Write(ctx, conn, &message{Content: msg}); err != nil {
-		h.log.Warnw("subscriber can not be notified", "error", err, "subscriberId", sId)
-	}
-}
-
 // Subscribe subscribes a client to the message channel to read and write messages from it
 func (h *Handlers) Subscribe(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	// Accept connection from the client
 	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		return err
@@ -255,28 +247,28 @@ func (h *Handlers) Subscribe(ctx context.Context, w http.ResponseWriter, r *http
 
 	s := &subscriber{
 		id:       r.RemoteAddr,
-		messages: make(chan any, h.subscriberMessageBuffer),
+		messages: make(chan *message, h.subscriberMessageBuffer),
 	}
 
 	h.addSubscriber(s)
 	defer h.deleteSubscriber(s)
 
-	h.notifySubscriber(ctx, conn, s.id, "wait for any available client to chat with...")
+	h.notifySubscriber(ctx, conn, s.id, "wait for any available subscriber to chat with...")
 
-	// Assign a pair to chat with.
+	// Assign a subscriber pair to chat with.
 	// When pair is successfully assigned, we can read messages from the pair and write messages to the pair
-	if err = h.assignAvailableSubscriber(ctx, s); err != nil {
-		h.log.Debugw("no subscriber pair is available", "subscriberId", s.id, "error", err)
-		h.notifySubscriber(ctx, conn, s.id, "no available clients to chat with; please, reload the page and try again later.")
+	if err = h.assignSubscriber(ctx, s); err != nil {
+		h.log.Debugw("no available subscribers", "subscriberId", s.id, "error", err)
+		h.notifySubscriber(ctx, conn, s.id, "no available subscribers; please, try again later.")
 
 		if err = conn.Close(websocket.StatusTryAgainLater, "no available subscribers"); err != nil {
-			h.log.Warnw("socket connection can not be closed", "error", err, "subscriberId", s.id)
+			h.log.Warnw("socket connection can not be closed", "subscriberId", s.id, "error", err)
 		}
 
 		return nil
 	}
 
-	h.notifySubscriber(ctx, conn, s.id, "available client is found.")
+	h.notifySubscriber(ctx, conn, s.id, "subscriber is found. Say Hi!")
 
 	// Read messages in a goroutine, ctx will be canceled if client closed the connection
 	ctx = h.readMessages(ctx, conn, s)
